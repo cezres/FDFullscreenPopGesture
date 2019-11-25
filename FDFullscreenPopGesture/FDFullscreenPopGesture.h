38b250e771ea8ca7b967630ad31d8e3075dd81c4
cezres <cezres@163.com>
2019-11-25 17:35:50 +0800

    chore: support carthage

---
+++ FDFullscreenPopGesture/FDFullscreenPopGesture.h
@@ -0,0 +1,19 @@
+//
+//  FDFullscreenPopGesture.h
+//  FDFullscreenPopGesture
+//
+//  Created by 翟泉 on 2019/11/25.
+//  Copyright © 2019 afantree. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for FDFullscreenPopGesture.
+FOUNDATION_EXPORT double FDFullscreenPopGestureVersionNumber;
+
+//! Project version string for FDFullscreenPopGesture.
+FOUNDATION_EXPORT const unsigned char FDFullscreenPopGestureVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FDFullscreenPopGesture/PublicHeader.h>
+
+#import "UINavigationController+FDFullscreenPopGesture.h"
